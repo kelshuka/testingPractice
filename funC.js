3d68bf0729301e467805802f5df189e73348713c
Ukam <ukam@Xubuntu.myguest.virtualbox.org>
2024-06-08 18:25:42 +0000

    Add package.json

--- funC.js
+++ funC.js
@@ -1,6 +1,16 @@
 
 function capitalize(str){
-    return str.toUpperCase();
+    return str.slice(0,1).toUpperCase() + str.slice(1);
 }
 
-export {capitalize};
+// the module pattern - IIFEs. wrap the factory function in () and invoke them
+const calculator = (function(){
+    const add = (a,b) => a+b;
+    const subtract = (a,b) => a-b;
+    const divide = (a,b) => a/b;
+    const multiply = (a,b) => a*b;
+
+    return {add, subtract, divide, multiply};
+})();
+
+export {capitalize, calculator};
--- funC.test.js
+++ funC.test.js
@@ -1,7 +1,31 @@
 
 import { capitalize } from "./funC";
+import { calculator } from "./funC";
 
 test("string is capitalized", () => {
-    //expect(capitalize('xedfgv')).toBe("Xedfgv");//error
-    expect(capitalize('xedfgv')).toBe("XEDFGV");
+    expect(capitalize('xedfgv')).toBe("Xedfgv");// or .toMatch
+    //expect(capitalize('xedfgv')).toBe("XEDFGV");//error
 });
+
+/* test("string is reversed", () => {
+    expect(reverseString('DervMenr')).toMatch('rneMvreD');
+});
+ */
+
+describe('calculator', () =>{
+    test('sum is 9', () =>{
+        expect(calculator.add(5,4)).toEqual(9);//or .toBe(9)
+    });
+
+    test('difference is 2', () =>{
+        expect(calculator.subtract(7,5)).toBe(2);
+    });
+
+    test('dividend is 4', () =>{
+        expect(calculator.divide(24,6)).toEqual(4);//or .toBe(9)
+    });
+
+    test('product is 32', () =>{
+        expect(calculator.multiply(16,2)).toEqual(32);//or .toBe(9)
+    });
+})
