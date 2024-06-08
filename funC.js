da411d407d81f9f6b95b3fa6ee319d288530108c
Ukam <ukam@Xubuntu.myguest.virtualbox.org>
2024-06-08 21:05:04 +0000

    TDD test practices and functions

--- funC.js
+++ funC.js
@@ -3,6 +3,10 @@ function capitalize(str){
     return str.slice(0,1).toUpperCase() + str.slice(1);
 }
 
+function reverseString(str){
+    return str.split("").reverse().join("");
+}
+
 // the module pattern - IIFEs. wrap the factory function in () and invoke them
 const calculator = (function(){
     const add = (a,b) => a+b;
@@ -13,4 +17,40 @@ const calculator = (function(){
     return {add, subtract, divide, multiply};
 })();
 
-export {capitalize, calculator};
+function caesarCipher(str, shift){
+    let ciphStr = '';
+
+    for(let i=0; i < str.length;i++){
+        let letterPosition = str[i].charCodeAt();
+
+        // Check if the character is a letter
+        if( (letterPosition >=65 && letterPosition <=90) || (letterPosition >=97 && letterPosition <=122) ){
+            letterPosition += shift;
+
+            // this is for letters, x,y and z, that are extreme that are replaced a-c
+            if( (letterPosition > 90 && letterPosition <= 'z') || letterPosition > 122 ){
+                letterPosition -= 26;
+            }
+        }
+        ciphStr += String.fromCharCode(letterPosition);
+    }
+
+    return ciphStr;
+}
+
+
+function analyzeArray(arr){
+    //let avg, mini, maxi, len;
+
+    const avg = arr.reduce( (total, currentItem) => total + currentItem,0)/arr.length;
+    let mini = arr[0], maxi = arr[0];
+    for(let i of arr){
+        if(i <= mini) mini = i;
+        if(i >= maxi) maxi = i;
+    }
+
+    return {average: avg, min: mini, max: maxi, length: arr.length};
+}
+
+export {capitalize, reverseString ,calculator, caesarCipher, analyzeArray};
+
--- funC.test.js
+++ funC.test.js
@@ -1,16 +1,22 @@
 
-import { capitalize } from "./funC";
-import { calculator } from "./funC";
+import { capitalize, reverseString, calculator } from "./funC"; 
+import { caesarCipher, analyzeArray } from "./funC";
 
 test("string is capitalized", () => {
-    expect(capitalize('xedfgv')).toBe("Xedfgv");// or .toMatch
-    //expect(capitalize('xedfgv')).toBe("XEDFGV");//error
+    expect(capitalize('xedfgv')).toBe("Xedfgv");
 });
 
-/* test("string is reversed", () => {
-    expect(reverseString('DervMenr')).toMatch('rneMvreD');
+describe('reverse function', () => {
+    test("string is reversed", () => {
+        expect(reverseString('DervMenr')).toMatch('rneMvreD');
+    });
+
+    test("space and other characters test", () => {
+        expect(reverseString('The world, view!')).toMatch('!weiv ,dlrow ehT');
+    });
 });
- */
+
+
 
 describe('calculator', () =>{
     test('sum is 9', () =>{
@@ -22,10 +28,51 @@ describe('calculator', () =>{
     });
 
     test('dividend is 4', () =>{
-        expect(calculator.divide(24,6)).toEqual(4);//or .toBe(9)
+        expect(calculator.divide(24,6)).toEqual(4);
     });
 
     test('product is 32', () =>{
-        expect(calculator.multiply(16,2)).toEqual(32);//or .toBe(9)
+        expect(calculator.multiply(16,2)).toEqual(32);
     });
-})
+});
+
+describe('Caesar Shift Cipher', () => {
+    test('is abc', () =>{
+        expect(caesarCipher('xyz',3)).toMatch('abc');
+    });
+
+    test('is fhj', () =>{
+        expect(caesarCipher('bdf',4)).toMatch('fhj');
+    });
+    test('Case sensitivie: is KhOOr', () =>{
+        expect(caesarCipher('HeLLo',3)).toMatch('KhOOr');
+    });
+    test('space, punctuation and other characters test: is Khoor, Zruog!', () =>{
+        expect(caesarCipher('Hello, World!',3)).toMatch('Khoor, Zruog!');
+    });
+    test('Shift three places to the left: is TLoiA', () =>{
+        expect(caesarCipher('WOrlD',-3)).toMatch('TLoiA');
+    });
+    test('no shift, is nothing', () =>{
+        expect(caesarCipher('nothing',0)).toMatch('nothing');
+    });   
+});
+
+describe('Analyze Array', () => {
+    test('is object', () =>{
+        expect(analyzeArray([3,4,5])).toBeInstanceOf(Object);
+    });
+
+    test('contain average', () =>{
+        expect(Object.keys( analyzeArray([3,4,5])) ).toContain("average");
+    });
+    test('return full object', () =>{
+        expect(analyzeArray([1,8,3,4,2,6])).toEqual({
+            "average":4, min:1, max:8, length: 6});
+    });
+    test('negative value test', () =>{
+        expect(analyzeArray([1,8,3,4,-3,2,6])).toEqual({
+            "average":3, min:-3, max:8, length: 7});
+    });
+   
+});
